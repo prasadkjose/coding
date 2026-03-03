8a8dd27494dbb5528e26facbfe6e361a171c2d0c
prasad koshy jose <prasadkjose@gmail.com>
2026-03-03 09:29:02 -0700

    Solution for kth smallest number

---
+++ arrays/kth_smallest/kth_smallest.py
@@ -0,0 +1,9 @@
+""" Solution Module """
+class Solution:
+    """ Solution Class """
+    def run_solution(self, test_input):
+        """ Solution Method """
+        [nums, k] = test_input
+        sorted_list = sorted(nums)
+
+        return sorted_list[k-1]
